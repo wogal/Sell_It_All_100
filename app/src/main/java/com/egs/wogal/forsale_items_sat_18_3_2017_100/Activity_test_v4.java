7f1ae78c19cb89a21f5c75f6a06c617a57ca9a44
wogal <egilgeous@gmail.com>
2017-03-20 04:42:03 -0400

    Initial commit

---
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_test_v4.java
@@ -0,0 +1,13 @@
+package com.egs.wogal.forsale_items_sat_18_3_2017_100;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_test_v4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.layout_v4 );
+    }
+}
