5352c36ecbf2183982050e3ef814ae3412e6acf5
wogal <egilgeous@gmail.com>
2017-03-25 18:24:38 -0400

    Saturday  _25 _march_06_21_pm --  working on image helpers most of the help is in Book Mark -> Android Code Stuf GOOD ->  Android Cam Api Stuff ->  camera intents - Part 6 -ReScale/Resize Image pici stuff works adding recycler  view ,, done Recycler View On Host -> layout_v6 ,, and Recycle Obj on v7

--- app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Recycle_v6.java
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Recycle_v6.java
@@ -14,6 +14,8 @@ public class Activity_Recycle_v6 extends AppCompatActivity implements View.OnCli
     private RecyclerView mRecView;
 
 
+
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
