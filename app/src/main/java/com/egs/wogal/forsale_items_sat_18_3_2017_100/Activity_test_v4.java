0d9dbbea5d3e090465b67584307d7c76b4856786
wogal <egilgeous@gmail.com>
2017-04-03 11:19:18 -0400

    most of the help is in Book Mark -> Android Code Stuf GOOD ->  Android Cam Api Stuff ->  camera intents - Part 6 -ReScale/Resize Image pici stuff works adding recycler  view ,, done Recycler View On Host -> layout_v6 ,, and Recycle Obj on v7 Moved classes / Objects into Logical Groups / Pkgs // SUNDAY ,, Start On Putting the bit to gather // and app integration starts // adding  Serialization  for sales item object on layout_v8 // interum commit // Monday 3/27/2017 == 12:06pm // worrking on item oject // monday 04:31pm //  last bits of sales item ( v8 ) // recycle on click [part ,, tuesday 3/28/2017 // friday 31st may // adding camera to item  content list v9
    // issue with camrea destroying event ,, ( ondestroy )
    // sorted above onDestroy issue
    // **************************************** //
    // Starting FaceBook dev interface
    // monday 4/3/2017
    // facebook logon with sell_It_All_100

--- app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_test_v4.java
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_test_v4.java
@@ -7,12 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+
 public class Activity_test_v4 extends AppCompatActivity implements View.OnClickListener {
 
     private Button mBut_test;
     private TextView m_txt_view;
     private Button mBut_Done;
     private Button mbut_Recyclerview;
+    private Button mBut_FacebookIntergration;
 
 
     @Override
@@ -31,8 +34,11 @@ public class Activity_test_v4 extends AppCompatActivity implements View.OnClickL
         mBut_Done = (Button) findViewById( R.id.But_Done_v4 );
         mBut_Done.setOnClickListener( this );
 
-        mbut_Recyclerview = (Button) findViewById( R.id.But_recyclerview_v6 );
+        mbut_Recyclerview = (Button) findViewById( R.id.But_recyclerview_v4 );
         mbut_Recyclerview.setOnClickListener( this );
+
+        mBut_FacebookIntergration = (Button) findViewById( R.id.But_facebookIntergration_v4 );
+        mBut_FacebookIntergration.setOnClickListener( this );
 
 
     }
@@ -40,11 +46,16 @@ public class Activity_test_v4 extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick (View v) {
         switch (v.getId()) {
+            case R.id.But_facebookIntergration_v4:{
+             Intent intentFaceBook_v10 = new Intent( this,Activity_FaceBook_v10.class );
+                startActivity( intentFaceBook_v10 );
+                break;
+            }
             case R.id.But_Done_v4: {
                 this.finish();
                 break;
             }
-            case R.id.But_recyclerview_v6: {
+            case R.id.But_recyclerview_v4: {
                 // test recyclerview
                 Intent intent = new Intent( this, Activity_Recycle_v6.class );
                 startActivity( intent );
@@ -58,5 +69,9 @@ public class Activity_test_v4 extends AppCompatActivity implements View.OnClickL
             }
         }
     }
+
+
+
+
 }
 
