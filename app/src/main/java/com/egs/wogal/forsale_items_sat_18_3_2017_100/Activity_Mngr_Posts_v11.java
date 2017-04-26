006e53e32806cdff3ec7360e89bb8ab4a568c36a
wogal <egilgeous@gmail.com>
2017-04-26 03:50:44 -0400

    // new Branches debugging mult image page post // sat 4/22/2017 ,, debuging -- " HlpFbook_Posts  " and renaming it to " FB_Post_ " to -> " FB_Post_Multi_Imges " // also making custom  post into sepeate class / file ( more later ) // new_branch_105 more work on FB_Posts monday 4/24/2017 // new_branch_106 ,, tuesday 4/25/2017 ,, working on posts and working on FB Post and ultilitys // new_branch_107 tue 4/25/2017 adding post group mangemnt // wed 3:00 am adding post manment

--- app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Mngr_Posts_v11.java
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Mngr_Posts_v11.java
@@ -2,12 +2,17 @@ package com.egs.wogal.forsale_items_sat_18_3_2017_100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class Activity_Mngr_Posts_v11 extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity__mngr__posts_v11 );
+        mRecyclerView = (RecyclerView) findViewById( R.id.Rec_Posts_v11 );
+
     }
 }
