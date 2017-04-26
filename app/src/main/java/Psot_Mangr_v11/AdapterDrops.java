cec71bf2898249b58629d414fc4124239492581a
wogal <egilgeous@gmail.com>
2017-04-26 05:34:27 -0400

    // new Branches debugging mult image page post // sat 4/22/2017 ,, debuging -- " HlpFbook_Posts  " and renaming it to " FB_Post_ " to -> " FB_Post_Multi_Imges " // also making custom  post into sepeate class / file ( more later ) // new_branch_105 more work on FB_Posts monday 4/24/2017 // new_branch_106 ,, tuesday 4/25/2017 ,, working on posts and working on FB Post and ultilitys // new_branch_107 tue 4/25/2017 adding post group mangemnt // wed 3:00 am adding post manment // new_branch_108 ,, start recycle ( file post lists )  layout v11

--- app/src/main/java/Psot_Mangr_v11/AdapterDrops.java
+++ app/src/main/java/Psot_Mangr_v11/AdapterDrops.java
@@ -1,17 +1,29 @@
 package Psot_Mangr_v11;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.egs.wogal.forsale_items_sat_18_3_2017_100.R;
 
 /**
  * Created by wogal on 4/26/2017.
  */
 
-public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder>{
+public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder> {
+    private LayoutInflater mInflater;
+
+    public AdapterDrops (Context _context) {
+        mInflater = LayoutInflater.from( _context );
+    }
+
     @Override
     public DropHolder onCreateViewHolder (ViewGroup parent, int viewType) {
-        return null;
+        View view = mInflater.inflate( R.layout.layout_rec_item_v7, parent, false );
+        DropHolder holder = new DropHolder( view );
+        return holder;
     }
 
     @Override
@@ -21,13 +33,13 @@ public class AdapterDrops extends RecyclerView.Adapter<AdapterDrops.DropHolder>{
 
     @Override
     public int getItemCount () {
-        return 0;
+        return 10;
     }
 
-    public static class DropHolder extends RecyclerView.ViewHolder{
+    public static class DropHolder extends RecyclerView.ViewHolder {
 
-      public DropHolder (View itemView) {
-          super( itemView );
-      }
-  }
+        public DropHolder (View itemView) {
+            super( itemView );
+        }
+    }
 }
--- app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Mngr_Posts_v11.java
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Mngr_Posts_v11.java
@@ -2,7 +2,10 @@ package com.egs.wogal.forsale_items_sat_18_3_2017_100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import Psot_Mangr_v11.AdapterDrops;
 
 public class Activity_Mngr_Posts_v11 extends AppCompatActivity {
     private RecyclerView mRecyclerView;
@@ -13,6 +16,9 @@ public class Activity_Mngr_Posts_v11 extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity__mngr__posts_v11 );
         mRecyclerView = (RecyclerView) findViewById( R.id.Rec_Posts_v11 );
+        LinearLayoutManager manger = new LinearLayoutManager( this );
+        mRecyclerView.setLayoutManager( manger );
+        mRecyclerView.setAdapter( new AdapterDrops( this ) );
 
     }
 }
