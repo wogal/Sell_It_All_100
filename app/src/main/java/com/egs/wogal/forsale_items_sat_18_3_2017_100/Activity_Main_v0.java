d907a89ddda90f11781449210a372b872b7f0384
wogal <egilgeous@gmail.com>
2017-04-27 13:17:07 -0400

    // new Branches debugging mult image page post // sat 4/22/2017 ,, debuging -- " HlpFbook_Posts  " and renaming it to " FB_Post_ " to -> " FB_Post_Multi_Imges " // also making custom  post into sepeate class / file ( more later ) // new_branch_105 more work on FB_Posts monday 4/24/2017 // new_branch_106 ,, tuesday 4/25/2017 ,, working on posts and working on FB Post and ultilitys // new_branch_107 tue 4/25/2017 adding post group mangemnt // wed 3:00 am adding post manment // new_branch_108 ,, start recycle ( file post lists )  layout //  new_branch_109 v11 xml code ,, layout v11

--- app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Main_v0.java
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Main_v0.java
@@ -30,37 +30,40 @@ public class Activity_Main_v0 extends AppCompatActivity implements View.OnClickL
         setContentView( R.layout.layout_v0 );
         mBtn_startselling_vn = (Button) findViewById( R.id.butt_lets_start_selling_v0 );
         mBtn_startselling_vn.setOnClickListener( this );
-         // invoke system initialize Actions
+        // invoke system initialize Actions
         Initialize_System( this );
 
     }
 
     private void Initialize_System (Activity _activity_main_v0) {
         // make sure system folders are set up
-        mBuilderItemName = new AlertDialog.Builder( Activity_Main_v0.this );
-        mViewItemName = getLayoutInflater().inflate( R.layout.layout_init_alrt_dialog_v12, null );
-        mBut_name_item_GoBack = (Button) mViewItemName.findViewById( R.id.But_item_name_done_v2 );
-        mTxtView_init_header_v12 = (TextView) findViewById( R.id.text_view_init_header_v12 );
-        mTxtView_init_action_v12 = (TextView) findViewById( R.id.text_view_init_actions_v12 );
 
 
-        mTxtView_init_header_v12.setText( "Init Actions" );
+        if (false ) {
 
-        if (false) {
+        } else {
+            mBuilderItemName = new AlertDialog.Builder( Activity_Main_v0.this );
+            mViewItemName = getLayoutInflater().inflate( R.layout.layout_init_alrt_dialog_v12, null );
+            mBut_name_item_GoBack = (Button) mViewItemName.findViewById( R.id.But_item_name_done_v12 );
+            mTxtView_init_header_v12 = (TextView) mViewItemName.findViewById( R.id.text_view_init_header_v12 );
+            mTxtView_init_action_v12 = (TextView) mViewItemName.findViewById( R.id.text_view_init_actions_v12 );
+            mTxtView_init_header_v12.setText( "Init Actions (v12)" );
+            // stops AlertDialog from dismissing on touch out side the AlertDialog
+            mBuilderItemName.setCancelable( false );
             mBut_name_item_GoBack.setOnClickListener( new View.OnClickListener() {
                 @Override
                 public void onClick (View v) {
                     switch (v.getId()) {
-                        case R.id.But_item_name_done_v2:
+                        case R.id.But_item_name_done_v12:
                             Dialog_ItemName.dismiss();
                             break;
                     }
                 }
             } );
+            mBuilderItemName.setView( mViewItemName );
+            Dialog_ItemName = mBuilderItemName.create();
+            Dialog_ItemName.show();
         }
-        mBuilderItemName.setView( mViewItemName );
-        Dialog_ItemName = mBuilderItemName.create();
-        Dialog_ItemName.show();
     }
 
     @Override
