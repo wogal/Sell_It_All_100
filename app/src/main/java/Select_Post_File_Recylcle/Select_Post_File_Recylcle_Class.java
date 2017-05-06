5c62a76f0427ca3f74af57bfa067deb7f849a105
wogal <egilgeous@gmail.com>
2017-05-06 18:04:30 -0400

    // new Branches debugging mult image page post // sat 4/22/2017 ,, debuging -- " HlpFbook_Posts  " and renaming it to " FB_Post_ " to -> " FB_Post_Multi_Imges " // also making custom  post into sepeate class / file ( more later ) // new_branch_105 more work on FB_Posts monday 4/24/2017 // new_branch_106 ,, tuesday 4/25/2017 ,, working on posts and working on FB Post and ultilitys // new_branch_107 tue 4/25/2017 adding post group mangemnt // wed 3:00 am adding post manment // new_branch_108 ,, start recycle ( file post lists )  layout //  new_branch_109 v11 xml code ,, layout v11 // new_branch_109_water_shed_post ,, // new_branch_110 ,, thursday 4:40 pm ,, 4/27/2017 -- consolodatingn aleard dialog // new_branch_111 ,, friday ,, adding work dialogs ,, and new post helpers // saturday adding Dialog_Timed_Notice_v15 ( for displaying status / errors ) // last post working then removing Voice Header from v8 ( and rationalition dialogs ie: removing unused items ) new_branch_114 ,, monday 5/1/2017 post item work // new_branch_115 ,, code Sale Post Details v15 // still working on post item uinput ,, also Diana's Resenation from the app and all future ventures ( voice recording included " Diana's Issues ") // new_branch_116 ,, tuesday 5/3/2017 ,, add Post Item details // sat 5/6/2017 ,, // new_branch_119 // new_branch_120 --- select post file recycle code v17

---
+++ app/src/main/java/Select_Post_File_Recylcle/Select_Post_File_Recylcle_Class.java
@@ -0,0 +1,8 @@
+package Select_Post_File_Recylcle;
+
+/**
+ * Created by wogal on 5/6/2017.
+ */
+
+public class Select_Post_File_Recylcle_Class {
+}
---
+++ app/src/main/java/com/egs/wogal/forsale_items_sat_18_3_2017_100/Activity_Select_Post_File_v17.java
@@ -0,0 +1,13 @@
+package com.egs.wogal.forsale_items_sat_18_3_2017_100;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Select_Post_File_v17 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity__select__post__file_v17 );
+    }
+}
